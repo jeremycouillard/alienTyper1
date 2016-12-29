93ef5d71f8370b9c61264ec7e9f94b6631dc72e0
Jeremy Couillard <jeremycouillard@gmail.com>
2016-12-29 14:50:09 -0500

    Update master_writer2.ino

--- master_writer2/master_writer2.ino
+++ master_writer2/master_writer2.ino
@@ -10,6 +10,8 @@ const byte solenoid1 = 8;
 const byte solenoid2 = 9;
 const byte OnOffSwitch = 2;
 
+//const int list[49];
+
 unsigned int typeTime; //how much time between keystrokes
 unsigned int delayBetweenTyping; //how much time between sentences
 
@@ -20,7 +22,7 @@ const char string_2[] PROGMEM = "Excel spreadsheets for kids.";
 const char string_3[] PROGMEM = "Do you know how many different shirts have been made since Jefferson was president?";
 const char string_4[] PROGMEM = "I hate cleaning cat litter.";
 const char string_5[] PROGMEM = "Fifteen holidays about misogyny.";
-const char string_6[] PROGMEM = "Lets get high and go to spa castle.";
+const char string_6[] PROGMEM = "Let's get high and go to spa castle.";
 const char string_7[] PROGMEM = "What does dango mean?";
 const char string_8[] PROGMEM = "The ancient tribe of yogurt.";
 const char string_9[] PROGMEM = "My bracelet is made out of carboplatin?";
@@ -50,11 +52,24 @@ const char string_32[] PROGMEM = "Smart cars have formed real estate companies a
 const char string_33[] PROGMEM = "How much did BMW pay you to decorate their car?";
 const char string_34[] PROGMEM = "It's been a terrible day.";
 const char string_35[] PROGMEM = "My spaceship is a gemzar";
+const char string_36[] PROGMEM = "My bathing suit was hand woven by the director of the CIA.";
+const char string_37[] PROGMEM = "Four hour infusion of electrolytes to join this cult.";
+const char string_38[] PROGMEM = "What did you get me for Christmas?";
+const char string_39[] PROGMEM = "I'm hungover from gummy worms.";
+const char string_40[] PROGMEM = "Is there seriously no hot tub at this hotel?";
+const char string_41[] PROGMEM = "How has Jeff been doing?";
+const char string_42[] PROGMEM = "I want to go to Easter Island but I don’t want to go with you.";
+const char string_43[] PROGMEM = "I've been sober for two million years.";
+const char string_44[] PROGMEM = "Maltodextrin injection at 6pm tonight.";
+const char string_45[] PROGMEM = "I have a job interview tomorrow to be a drug dealer.";
+const char string_46[] PROGMEM = "It's my flying carpet’s birthday tomorrow and I'm taking her to 6 flags.";
+const char string_47[] PROGMEM = "Intergalactic war for Kleenex.";
+const char string_48[] PROGMEM = "There is a video game the size of Neptune.";
+const char string_49[] PROGMEM = "My best friend the functionary.";
 
 // Then set up a table to refer to your strings.
-const char* const string_table[] PROGMEM = {string_0, string_1, string_2, string_3, string_4, string_5, string_6,string_7,string_8,string_9, string_10, string_11, string_12, string_13, string_14, string_15, string_16, string_17, string_18, string_19, string_20, string_21, string_22, string_23, string_24, string_25, string_26, string_27, string_28, string_29, string_30, string_31, string_32, string_33, string_34, string_35};
-
-char buffer[90];    // make sure this is large enough for the largest string it must hold
+const char* const string_table[] PROGMEM = {string_0, string_1, string_2, string_3, string_4, string_5, string_6, string_7, string_8, string_9, string_10, string_11, string_12, string_13, string_14, string_15, string_16, string_17, string_18, string_19, string_20, string_21, string_22, string_23, string_24, string_25, string_26, string_27, string_28, string_29, string_30, string_31, string_32, string_33, string_34, string_35, string_36, string_37, string_38, string_39, string_40, string_41, string_42, string_43, string_44, string_45, string_46, string_47, string_48, string_49};
+char buffer[95];    // make sure this is large enough for the largest string it must hold
 
 void setup()
 {
@@ -70,48 +85,55 @@ void typeTheSentence() {
     
   delayBetweenTyping = random(1000, 2001); //2000, 6001 normally
   
-  //pull a sentence from the array
-  int randString = random(0, sizeof(string_table) / sizeof(string_table[0]));
-  const char * newGuy = strcpy_P(buffer, (char*)pgm_read_word(&(string_table[randString])));
+//make an ordered list
+int list[] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49};
+//and then shuffle it
+  for (int a = 0; a < 49; a++){
+    int r = random(a, 49);
+    int temp = list[a];
+    list[a] = list[r];
+    list[r] = temp;
+    }
+//pull from the list in the order      
+  for (int i = 0; i < 49; i++){
+    const char * newGuy = strcpy_P(buffer, (char*)pgm_read_word(&(string_table[list[i]])));
   
-  //get the length of the sentence pulled
-  int stringSize = strlen(newGuy);
-  Keyboard.begin();
-  //type out letter by letter
-  for (int i = 0; i < stringSize; i++) {
-    typeTime = random(20, 201);
-    Keyboard.print(newGuy[i]);
+    //get the length of the sentence pulled
+    int stringSize = strlen(newGuy);
+     Keyboard.begin();
+    //type out letter by letter
+      for (int i2 = 0; i2 < stringSize; i2++) {
+        typeTime = random(20, 201);
+        Keyboard.print(newGuy[i2]);
+        digitalWrite(solenoid1, LOW);
+        digitalWrite(solenoid2, HIGH);
+        delay(typeTime);
+        digitalWrite(solenoid1, HIGH);
+        digitalWrite(solenoid2, LOW);
+        delay(typeTime);
+      }
+      
+    //done with that sentence. put hands down and stop typing.
     digitalWrite(solenoid1, LOW);
-    digitalWrite(solenoid2, HIGH);
-    delay(typeTime);
-    digitalWrite(solenoid1, HIGH);
     digitalWrite(solenoid2, LOW);
-    delay(typeTime);
-  }
-  //done with that sentence. put hands down and stop typing.
-  digitalWrite(solenoid1, LOW);
-  digitalWrite(solenoid2, LOW);
-  Keyboard.press(KEY_RETURN);
-  Keyboard.releaseAll();
-  Keyboard.end();
-  delay(delayBetweenTyping);
-}
+    Keyboard.press(KEY_RETURN);
+    Keyboard.releaseAll();
+    Keyboard.end();
+    delay(delayBetweenTyping);
+    
+     //if off switch is hit break the list loop 
+     if (digitalRead(OnOffSwitch) == HIGH){
+      break; 
+    }
+  } //end for loop of list
+ 
+ 
+} //end function
+
 
 void loop() {
-  if (digitalRead(OnOffSwitch) == HIGH) typeTheSentence();  //do nothing while pin 2 does not go LOW
+  if (digitalRead(OnOffSwitch) == LOW) { //do nothing while pin 2 does not go LOW
+    typeTheSentence();  
+  }
   delay(30); // wait a bit, not really necessary
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
